62e08fb2a7b19e74a9d91903a3dd4657568f394f
wintaraputra242 <wintaraputra242@gmail.com>
2025-12-11 20:58:02 +0700

    feat: add new page my activity

--- layouts/components/UserProfile.vue
+++ layouts/components/UserProfile.vue
@@ -68,7 +68,7 @@ import avatar1 from '@images/avatars/avatar-1.png';
           </VListItem>
 
           <!-- 👉 Settings -->
-          <VListItem link>
+          <VListItem link to="/my-activity">
             <template #prepend>
               <VIcon
                 class="me-2"
